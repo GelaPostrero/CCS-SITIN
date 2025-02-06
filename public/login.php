99b11f1b55671a07b4f3716e0a479ecc225b8e3c
GelaPostrero <angelapostrero@gmail.com>
2025-02-06 13:23:59 +0800

    new commit

--- public/login.php
+++ public/login.php
@@ -8,7 +8,6 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $username = filter_input(INPUT_POST, 'username', FILTER_SANITIZE_STRING);
     $password = filter_input(INPUT_POST, 'password', FILTER_SANITIZE_STRING);
 
-    // Prepare SQL statement
     $sql = "SELECT * FROM users WHERE username = ?";
     $stmt = $conn->prepare($sql);
     $stmt->bind_param("s", $username);
@@ -39,40 +38,62 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://www.phptutorial.net/app/css/style.css">
-    <link rel="stylesheet" href="login.css">
+    <link rel="stylesheet" href="fonts/material-design-iconic-font/css/material-design-iconic-font.min.css">
+    <link rel="stylesheet" href="css/login.css">
     <title>Log In</title>
+    <style>
+        .image-holder{
+            flex-direction: column;
+            justify-content: center;
+            align-items: center;
+        }
+        .image-holder .logo{
+            display: flex;
+            justify-content: center;
+            align-items: center;
+        }
+        .image-holder .logo h1{
+            font-size: 25px;
+        }
+        .image-holder .logo img{
+            width: 20%;
+        }
+        :root {
+            --input-background-color: none;
+        }
+        input {
+            background-color: var(--input-background-color);
+        }
+    </style>
 </head>
 <body>
-<main>
-    <div class="left-con">
-        <div class="logo">
-            <img src="inc/CCS_LOGO.png" alt="CCS LOGO">
-            <div style="width: 60%;"><h1>SIT-IN MONITORING SYSTEM</h1></div>
-        </div>
-        <img src="inc/graphs.svg" alt="graphics" style="width: 95%;">
-    </div>
-    <div class="right-con"> 
-        <form action="login.php" method="post">
-            <div class="form-header">
-                <h2>Log in to your Account</h2>
+<div class="wrapper" style="background-image: url('inc/computer.png');">
+        <div class="inner">
+            <div class="image-holder">
+                <div class="logo">
+                    <img src="inc/CCS_LOGO.png" alt="CCS LOGO">
+                    <div style="width: 60%;"><h1>SIT-IN MONITORING SYSTEM</h1></div>
+                </div>
+                <img src="inc/graphs.svg" alt="graphics" style="width: 95%;">
+            </div>
+            <form action="login.php" method="post">
+                <h3>Sign In</h3>
                 <p>Welcome back! Please enter your details.</p>
-            </div>
-            <?php if ($error): ?>
-                <div style="color: red;"><?php echo htmlspecialchars($error); ?></div>
-            <?php endif; ?>
-            <div>
-                <label for="username">Username*</label>
-                <input type="text" placeholder="Enter your username" name="username" id="username" required>
-            </div>
-            <div>
-                <label for="password">Password*</label>
-                <input type="password" placeholder="Enter password" name="password" id="password" required>
-            </div>
-            <button type="submit">Sign In</button>
-            <footer style="font-size: 16px; margin-top: 15px;">Don’t have an account? <a href="register.php">Sign Up here</a></footer>
-        </form>
+                <?php if ($error): ?>
+                    <div style="color: red;"><?php echo htmlspecialchars($error); ?></div>
+                <?php endif; ?>
+                <div class="form-wrapper">
+                    <input type="text" name="username" id="username" required placeholder="Username" class="form-control">
+                    <i class="zmdi zmdi-account"></i>
+                </div>
+                <div class="form-wrapper">
+                    <input type="password" name="password" id="password" required placeholder="Password" class="form-control">
+                    <i class="zmdi zmdi-lock"></i>
+                </div>
+                <button type="submit" style="margin-top: 0;">Sign In</button>
+                <footer>Don’t have an account? <a href="register.php">Sign Up here</a></footer>
+            </form>
+        </div>
     </div>
-
-</main>
 </body>
 </html>
--- public/register.php
+++ public/register.php
@@ -12,7 +12,6 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $username = filter_input(INPUT_POST, 'username', FILTER_SANITIZE_STRING);
     $password = password_hash($_POST['password'], PASSWORD_DEFAULT);
     
-    // Check if username or email already exists
     $check_sql = "SELECT * FROM users WHERE username = ? OR email = ?";
     $stmt = $conn->prepare($check_sql);
     $stmt->bind_param("ss", $username, $email);
@@ -22,7 +21,6 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     if ($result->num_rows > 0) {
         echo "<p style='color:red;'>Username or Email already exists!</p>";
     } else {
-        // Insert user into database
         $sql = "INSERT INTO users (idno, lastname, firstname, middlename, course, level, email, username, password) 
                 VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)";
         $stmt = $conn->prepare($sql);
@@ -45,65 +43,97 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://www.phptutorial.net/app/css/style.css">
+    <link rel="stylesheet" href="fonts/material-design-iconic-font/css/material-design-iconic-font.min.css">
+    <link rel="stylesheet" href="css/style.css">
     <title>Register</title>
+    <style>
+        .image-holder{
+            flex-direction: column;
+            justify-content: center;
+            align-items: center;
+        }
+        .image-holder .logo{
+            display: flex;
+            justify-content: center;
+            align-items: center;
+        }
+        .image-holder .logo h1{
+            font-size: 25px;
+        }
+        .image-holder .logo img{
+            width: 20%;
+        }
+        :root {
+            --input-background-color: none;
+        }
+        input {
+            background-color: var(--input-background-color);
+        }
+    </style>
 </head>
 <body>
-<main>
-    <form action="register.php" method="post">
-        <h1>Sign Up</h1>
-        <div>
-            <label for="idno">ID Number</label>
-            <input type="number" name="idno" id="idno" required>
+    <div class="wrapper" style="background-image: url('inc/computer.png');">
+        <div class="inner">
+            <div class="image-holder">
+                <div class="logo">
+                    <img src="inc/CCS_LOGO.png" alt="CCS LOGO">
+                    <div style="width: 60%;"><h1>SIT-IN MONITORING SYSTEM</h1></div>
+                </div>
+                <img src="inc/graphs.svg" alt="graphics" style="width: 95%;">
+            </div>
+            <form action="register.php" method="post">
+                <h3>Sign Up</h3>
+                <div class="form-wrapper">
+                    <input type="number" name="idno" id="idno" placeholder="ID Number" class="form-control" required >
+                    <i class="zmdi zmdi-card"></i>
+                </div>
+                <div class="form-group">
+                    <input type="text" name="lastname" id="lastname" required placeholder="Last Name" class="form-control">
+                    <input type="text" name="firstname" id="firstname" required placeholder="First Name" class="form-control" style="margin-right: 25px;">  
+                    <input type="text" name="middlename" id="middlename" placeholder="Middle Name" class="form-control">
+                </div>
+                <div class="form-group">
+                    <div class="form-wrapper" style="width: 50%; margin-right: 25px;">
+                        <select nid="course" name="course" required class="form-control">
+                            <option value="" disabled selected>Course</option>
+                            <option value="BSIT">BSIT</option>
+                            <option value="BSCS">BSCS</option>
+                        </select>
+                        <i class="zmdi zmdi-caret-down" style="font-size: 17px; bottom: 30px;"></i>
+                    </div>
+                    <div class="form-wrapper" style="width: 50%;">
+                        <select id="level" name="level" required required class="form-control">
+                            <option value="" disabled selected>Year Level</option>
+                            <option value="1">1</option>
+                            <option value="2">2</option>
+                            <option value="3">3</option>
+                            <option value="4">4</option>
+                        </select>
+                        <i class="zmdi zmdi-caret-down" style="font-size: 17px; bottom: 30px;"></i>
+                    </div>
+                </div>
+                <div class="form-wrapper">
+                    <input type="email" name="email" id="email" required placeholder="Email Address" class="form-control">
+                    <i class="zmdi zmdi-email"></i>
+                </div>
+                <div class="form-wrapper">
+                    <input type="text" name="username" id="username" required placeholder="Username" class="form-control">
+                    <i class="zmdi zmdi-account"></i>
+                </div>
+                <div class="form-wrapper">
+                    <input type="password" name="password" id="password" required placeholder="Password" class="form-control">
+                    <i class="zmdi zmdi-lock"></i>
+                </div>
+                <div>
+                    <label for="agree">
+                        <input type="checkbox" name="agree" id="agree" required> I agree
+                        with the <a href="#" title="terms of service">terms of service</a>
+                    </label>
+                </div>
+                <button type="submit" style="margin-top: 0;">Register</button>
+                <footer>Already have an account? <a href="login.php">Login here</a></footer>
+            </form>
         </div>
-        <div>
-            <label for="lastname">Last Name</label>
-            <input type="text" name="lastname" id="lastname" required>
-        </div>
-        <div>
-            <label for="firstname">First Name</label>
-            <input type="text" name="firstname" id="firstname" required>
-        </div>
-        <div>
-            <label for="middlename">Middle Name</label>
-            <input type="text" name="middlename" id="middlename">
-        </div>
-        <div>
-            <label for="course">Course</label>
-            <select id="course" name="course" required>
-                <option value="BSIT">BSIT</option>
-                <option value="BSCS">BSCS</option>
-            </select>
-        </div>
-        <div>
-            <label for="level">Year Level</label>
-            <select id="level" name="level" required>
-                <option value="1">1st Year</option>
-                <option value="2">2nd Year</option>
-                <option value="3">3rd Year</option>
-                <option value="4">4th Year</option>
-            </select>
-        </div>
-        <div>
-            <label for="email">Email Address</label>
-            <input type="email" name="email" id="email" required>
-        </div>
-        <div>
-            <label for="username">Username</label>
-            <input type="text" name="username" id="username" required>
-        </div>
-        <div>
-            <label for="password">Password</label>
-            <input type="password" name="password" id="password" required>
-        </div>
-        <div>
-            <label for="agree">
-                <input type="checkbox" name="agree" id="agree" required> I agree
-                with the <a href="#" title="terms of service">terms of service</a>
-            </label>
-        </div>
-        <button type="submit">Register</button>
-        <footer>Already have an account? <a href="login.php">Login here</a></footer>
-    </form>
-</main>
+    </div>
 </body>
 </html>
